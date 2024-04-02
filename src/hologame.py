7ee547835171f5fe89a3d99bd4884b5dac06ea2b
nloncar177 <nloncar177@gmail.com>
2024-04-02 19:56:33 +0200

    Pocetni kontroler za platformer

--- src/hologame.py
+++ src/hologame.py
@@ -1,35 +1,71 @@
-# title:   Hologame-V
-# author:  Leonardo Andrasic
-# desc:    Igra za PRRI
-# site:    website link
-# license: MIT License (change this to your license of choice)
-# version: 0.1
-# script:  python
+-- title:   Hologame-V
+-- author:  Leonardo Andrasic
+-- desc:    Igra za PRRI
+-- site:    website link
+-- license: MIT License (change this to your license of choice)
+-- version: 0.1
+-- script:  python 
+
 
 t=0
 x=96
 y=24
 
-def TIC():
- global t
- global x
- global y
 
- if btn(0): y-=1
- if btn(1): y+=1
- if btn(2): x-=1
- if btn(3): x+=1
+minY=120 --najniza tocka
+minX=225 --najdesnija tocka
 
- cls(13)
- spr(
-  1+t%60//30*2,
-  x,y,
-  colorkey=14,
-  scale=3,
-  w=2,h=2
- )
- print("Hologame-V!",84,84)
- t+=1
+
+--Osnovne Varijable
+brzina=3
+gravitacija=2
+
+
+--Varijable skakanja
+skokVar=0 
+skokTrajanje=20
+skokJacina=2
+
+
+function TIC()
+
+ --skakanje
+ if btn(0) then if y==minY then 
+	skokVar=skokTrajanje 
+	end print() end
+	
+	if key(48) then if y==minY then 
+	skokVar=skokTrajanje 
+	end print() end
+	
+	if skokVar>0 then
+	y=y-skokJacina
+	skokVar=skokVar-1 
+	end
+	
+	
+	--kretanje lijevo desno
+	if btn(2) then x=x-brzina end
+	if btn(3) then x=x+brzina end
+	
+	
+ -- gravitacija
+	if y<minY then if skokVar<1
+	 then y=y+gravitacija end
+	else y=minY end
+
+	
+	--blokiranje lijevo i desno
+	if x>minX then x=minX end
+ if x<0 then x=0 end
+	
+	
+	--nez
+	cls(13)
+	spr(1+t%60//30*2,x,y,14,1,0,0,2,2)
+	t=t+1
+	
+end
 
 # <TILES>
 # 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
