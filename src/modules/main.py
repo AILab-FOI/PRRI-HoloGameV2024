4dc04e89c2b4c44d8d5cb510816f6b073859971a
landrasic <leonardo.andrasic@datamir.info>
2024-04-09 11:28:49 +0200

    Konverzija koda iz Lue u Python
    Dodan novi modul PlayerKontroler koji se ubacuje u glavni program.

--- src/modules/main.py
+++ src/modules/main.py
@@ -11,25 +11,7 @@ x=96
 y=24
 
 def TIC():
- global t
- global x
- global y
-
- if btn(0): y-=1
- if btn(1): y+=1
- if btn(2): x-=1
- if btn(3): x+=1
-
- cls(13)
- spr(
-  1+t%60//30*2,
-  x,y,
-  colorkey=14,
-  scale=3,
-  w=2,h=2
- )
- print("HoloGameP" + str(test(1,1))),84,84)
- t+=1
+ PlayerKontroler()
 
 # <TILES>
 # 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
