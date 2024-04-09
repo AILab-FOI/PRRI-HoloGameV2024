03fb12c3fcfc948b05c2580ae28ba65cfb59fa7f
roko9765 <roko9765@gmail.com>
2024-04-09 16:36:21 +0200

    Enemy i pokusaj projektila

--- src/hologamev.py
+++ src/hologamev.py
@@ -10,8 +10,31 @@ t=0
 x=96
 y=24
 
+class enemy:
+  x = 200  # Initial position (adjust as needed)
+  y = 120
+  sprite = 1 # Replace with the enemy's sprite ID
+  dx = -1   # Enemy moves left initially
+  dy = 0
+  desno = False
+
+class projektil:
+ def __init__(self, x, y):  # konstruktor
+    self.x = x
+    self.y = y
+    self.dx = 1 
+    self.dy = 0
+    self.speed = 5
+    
+
+
+
 def TIC():
+ Final()
  PlayerKontroler()
+ enemyMovement()
+ pucanjeProjektila()
+ 
 
 
 t=0
@@ -20,6 +43,26 @@ y=24
 
 minY=120 #najniza tocka
 minX=225 #najdesnija tocka
+
+shotTimer = 0
+def enemyMovement():
+ enemy.x = enemy.x + enemy.dx
+ enemy.y = enemy.y + enemy.dy
+ if enemy.x <= 0:
+    enemy.dx = 1  # mijenja stranu kad takne lijevu stranu
+    enemy.desno = True
+ elif enemy.x >= minX:
+    enemy.dx = -1  # mijenja stranu kad takne desnu stranu
+    enemy.desno = False
+
+ global shotTimer
+ shotTimer += 1 # timer se povecava se svaki frame
+
+ # puca svakih 5 sekundu
+ if shotTimer >= 60 * 5:
+    pucanjeProjektila()  # pozivanje funkcije
+    shotTimer = 0  # nakon poziva resetira se timer
+
 
 
 #Osnovne Varijable
@@ -67,14 +110,39 @@ def PlayerKontroler():
 		
     if x<0:
 		x=0
+          
+
+def pucanjeProjektila():
+  # krira objekt
+  projectile = projektil(enemy.x + 5, enemy.y)  # Adjust offset for visual clarity
+
+  # dodavanje projektila u listu
+  projectiles.append(projectile)
+
+  # lista za spremanje projektila
+projectiles = []
+ 
+
 
 
 	#ispis
-	cls(13)
-	spr(1+t%60//30*2,x,y,14,1,0,0,2,2)
-	t=t+1
-def test( a, b ):
-    return a+b
+def Final():
+    cls(13)
+    
+    #projektil
+    for projectile in projectiles:
+        spr(80,projectile.x,projectile.y,1,1,0,0,1,1)
+
+
+    #enemy    
+    if enemy.desno==True:
+        spr(1+t%60//30*2,enemy.x,enemy.y,14,1,1,0,2,2)
+    else:
+        spr(1+t%60//30*2,enemy.x,enemy.y,14,1,0,0,2,2)
+
+    #igrac    
+    spr(1+t%60//30*2,x,y,14,1,1,0,2,2) 
+    t=t+1
 # <TILES>
 # 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
 # 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
@@ -84,6 +152,7 @@ def test( a, b ):
 # 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
 # 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
 # 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+# 080:ddd22ddddd2222ddd22dd22d22d22d2222d22d22d22dd22ddd2222ddddd22ddd
 # </TILES>
 
 # <WAVES>
