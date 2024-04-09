2784c08197edc96e77256d14c42d61d4b7720ef3
roko9765 <roko9765@gmail.com>
2024-04-09 20:04:02 +0200

    Prvi enemy s implementiranim auto pucanjem
    Prvi enemy koji se krece lijevo dok ne dođe do kraja zaslona te onda mijenja smjer. Tako vrijedi i za desnu stranu. Također, ima implementiran sustav auto pucanja.

--- src/hologamev.py
+++ src/hologamev.py
@@ -11,147 +11,116 @@ x=96
 y=24
 
 class enemy:
-  x = 200  # Initial position (adjust as needed)
+  x = 200  
   y = 120
-  sprite = 1 # Replace with the enemy's sprite ID
-  dx = -1   # Enemy moves left initially
+  sprite = 1  
+  dx = -1  
   dy = 0
   desno = False
 
-class projektil:
- def __init__(self, x, y):  # konstruktor
+class Projectile:
+  def __init__(self, x, y):  # konstruktor klase
     self.x = x
     self.y = y
     self.dx = 1 
     self.dy = 0
-    self.speed = 5
-    
-
-
+    self.speed = 5  # brzina projektila
+    self.desno = True
 
 def TIC():
- Final()
- PlayerKontroler()
- enemyMovement()
- pucanjeProjektila()
- 
-
-
-t=0
-x=96
-y=24
+  Final()
+  PlayerKontroler()
+  enemyMovement()
+  Projektili()
 
 minY=120 #najniza tocka
 minX=225 #najdesnija tocka
 
-shotTimer = 0
+shotTimer = 0  # timer za pucanje
 def enemyMovement():
- enemy.x = enemy.x + enemy.dx
- enemy.y = enemy.y + enemy.dy
- if enemy.x <= 0:
+  enemy.x = enemy.x + enemy.dx
+  enemy.y = enemy.y + enemy.dy
+  if enemy.x <= 0:
     enemy.dx = 1  # mijenja stranu kad takne lijevu stranu
     enemy.desno = True
- elif enemy.x >= minX:
+  elif enemy.x >= minX:
     enemy.dx = -1  # mijenja stranu kad takne desnu stranu
     enemy.desno = False
 
- global shotTimer
- shotTimer += 1 # timer se povecava se svaki frame
+  global shotTimer
+  shotTimer += 1  # svaki frame se povecava za 1
 
- # puca svakih 5 sekundu
- if shotTimer >= 60 * 5:
-    pucanjeProjektila()  # pozivanje funkcije
-    shotTimer = 0  # nakon poziva resetira se timer
+  # puca svakih dvije sekunde
+  if shotTimer >= 60 * 2:
+    shootProjectile()  # poziv funkcije za pucanje
+    shotTimer = 0  # resetiranje timera
 
 
-
-#Osnovne Varijable
+# Osnovne Varijable
 brzina=3
 gravitacija=2
 
 
-#Varijable skakanja
+# Varijable skakanja
 skokVar=0 
 skokTrajanje=20
 skokJacina=2
 
 def PlayerKontroler():
-    global t, x, y, minY, minX, brzina, gravitacija, skokVar, skokTrajanje, skokJacina
+  global t, x, y, minY, minX, brzina, gravitacija, skokVar, skokTrajanje, skokJacina
+  
 
-     #skakanje
-    if btn(0):
-	    if y==minY:
-		    skokVar=skokTrajanje 
-                
-    if key(48):
-        if y==minY:
-            skokVar=skokTrajanje 
-    if skokVar>0:
-        y=y-skokJacina
-        skokVar=skokVar-1 
+def shootProjectile():
+  # kreiranje projektila
+  projectile = Projectile(enemy.x + 5, enemy.y) 
 
-    #kretanje lijevo desno
-    if btn(2): 
-        x=x-brzina
-    if btn(3):
-        x=x+brzina
-
-    #gravitacija
-	if y<minY:
-		if skokVar<1:
-		    y=y+gravitacija
-	else: 
-		y=minY
-
-
-	#blokiranje lijevo i desno
-	if x>minX:
-		x=minX
-		
-    if x<0:
-		x=0
-          
-
-def pucanjeProjektila():
-  # krira objekt
-  projectile = projektil(enemy.x + 5, enemy.y)  # Adjust offset for visual clarity
-
-  # dodavanje projektila u listu
+  projectile.desno = enemy.desno
+  # doda projektil u listu
   projectiles.append(projectile)
+  
+  
+def Projektili():
+  for projektil in projectiles:
+    if projektil.desno == True:
+      projektil.x = projektil.x + projektil.speed
+    else:
+      projektil.x = projektil.x - projektil.speed
 
-  # lista za spremanje projektila
+    if projektil.x < 0 or projektil.x > 240:
+     del projektil
+
+#lista projektila
 projectiles = []
- 
 
 
 
-	#ispis
+
 def Final():
     cls(13)
-    
-    #projektil
+
     for projectile in projectiles:
-        spr(80,projectile.x,projectile.y,1,1,0,0,1,1)
+     spr(80, projectile.x, projectile.y, 14, 1, 0, 0, 1, 1)
 
-
-    #enemy    
     if enemy.desno==True:
-        spr(1+t%60//30*2,enemy.x,enemy.y,14,1,1,0,2,2)
+        spr(5,enemy.x,enemy.y,14,1,1,0,2,2)
     else:
-        spr(1+t%60//30*2,enemy.x,enemy.y,14,1,0,0,2,2)
+        spr(5,enemy.x,enemy.y,14,1,0,0,2,2)
 
-    #igrac    
-    spr(1+t%60//30*2,x,y,14,1,1,0,2,2) 
+    spr(1+t%60//30*2,x,y,14,1,1,0,2,2)
     t=t+1
 # <TILES>
 # 001:eccccccccc888888caaaaaaaca888888cacccccccacc0ccccacc0ccccacc0ccc
 # 002:ccccceee8888cceeaaaa0cee888a0ceeccca0ccc0cca0c0c0cca0c0c0cca0c0c
 # 003:eccccccccc888888caaaaaaaca888888cacccccccacccccccacc0ccccacc0ccc
 # 004:ccccceee8888cceeaaaa0cee888a0ceeccca0cccccca0c0c0cca0c0c0cca0c0c
+# 005:eccccccccceeeeeec6666666c6eeeeeec6b00bbbc6bb00b0c6bb20b0c00b2bbb
+# 006:ccccceeeeeeeccee66665ceeeee65cee00b65ccc0bb65c0c2bb65c0c2bb65c0c
 # 017:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
 # 018:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
 # 019:cacccccccaaaaaaacaaacaaacaaaaccccaaaaaaac8888888cc000cccecccccec
 # 020:ccca00ccaaaa0ccecaaa0ceeaaaa0ceeaaaa0cee8888ccee000cceeecccceeee
+# 021:c00bbbbbc2006666c2666000c6666000c6666666ceeeeeeecc555cccecccccec
+# 022:bb0055cc60065cce66265cee66265cee66665ceeeeeeccee555cceeecccceeee
 # 080:ddd22ddddd2222ddd22dd22d22d22d2222d22d22d22dd22ddd2222ddddd22ddd
 # </TILES>
 
