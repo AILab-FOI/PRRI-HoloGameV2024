9e056b77de63df9120e460e269ef8876feb4dff1
David Matijanić <dmatijani21@foi.hr>
2024-04-23 22:59:28 +0200

    Izgrađen hologamev.py jer je onaj bio pokidan kod mergeanja (namjerno)

--- src/hologamev.py
+++ src/hologamev.py
@@ -7,6 +7,26 @@
 # script:  python
 
 t=0
+state='menu' #varijabla za game state
+
+def TIC():
+ Final()
+
+ global state
+ if state=='game':
+   cls(0)
+
+   map(0, 0, 36, 18, -int(pogled.x), -int(pogled.y), 0)
+
+   collidables = DefinirajKolizije([player, enemy])
+   enemy.movement(enemy, collidables)
+   for projektil in projectiles:
+      projektil.movement()
+   Pucanje()
+   player.PlayerKontroler(player, collidables)
+   pogled.pratiIgraca()
+ elif state=='menu':
+   menu.Menu()
 
 def Final():
 	cls(13)
@@ -291,6 +311,60 @@ class Projectile:
       self.x = self.x - self.speed
 
     #crtanje sebe
+    spr(80, self.x - int(pogled.x), self.y - int(pogled.y), 14, 1, 0, 0, 1, 1)
+
+    #brisanje ako se unisti
+    if self.x < 0 or self.x > pogled.ogranicenjeX:
+      del self
+     
+
+
+class menu:
+    m_ind=0
+
+    def Menu():
+        global state
+        cls(0)
+        menu.AnimateFrame()
+        menu.AnimateTitle()
+
+        # Opcije menija
+        rect(1,48+10*menu.m_ind,238,10,2)
+        print('Play', 100, 50, 4, False, 1, False)
+        print('Quit', 100, 60, 4, False, 1, False)
+
+        #  Šetanje po opcijama na meniju
+        if btnp(1) and 48+10*menu.m_ind<50: #ako se budu dodavale još koje opcije, promijeniti uvijet
+            menu.m_ind += 1
+        elif btnp(0) and 48+10*menu.m_ind>=50:
+            menu.m_ind += -1
+
+        # Odabir 
+        if key(48) and menu.m_ind==0:
+            state = 'game'
+        elif key(48) and menu.m_ind==1:
+            exit()
+
+    def AnimateTitle():
+        if(time()%500>250):
+            print('NEON ESCAPE', 57, 20, 6, False, 2, False)
+        elif(time()%500>150):
+            print('NEON ESCAPE', 57, 20, 2, False, 2, False)
+        elif(time()%500>350):
+            print('NEON ESCAPE', 57, 20, 3, False, 2, False)
+        elif(time()%500>550):
+            print('NEON ESCAPE', 57, 20, 10, False, 2, False)
+
+    def AnimateFrame():
+        if(time()%500>250):
+            rectb(0,0,240,136,6)
+        elif(time()%500>150):
+            rectb(0,0,240,136,2)
+        elif(time()%500>350):
+            rectb(0,0,240,136,3)
+        elif(time()%500>550):
+            rectb(0,0,240,136,10)
+
 
 def lerp(a, b, t):
     return (1-t)*a + t*b
