3ef25017c1560b8da916413c272dc173699625fc
David Matijanić <dmatijani21@foi.hr>
2024-04-23 15:33:23 +0200

    #44 Pogled je ograničen lijevo

--- src/hologamev.py
+++ src/hologamev.py
@@ -8,6 +8,7 @@
 
 t=0
 
+
 def TIC():
  Final()
  
@@ -310,10 +311,19 @@ class Pogled:
     y = 0
     w = 240
     h = 136
+    ograniceno = False
+    ogranicenjeX = 0
+
+    def __init__(self):
+        self.postaviOgranicenja(1000)
 
     def prati(self, objekt):
         self.x = objekt.x - (self.w - objekt.width)/2
         #self.y = objekt.y - (self.h - objekt.height)/2
+
+    def postaviOgranicenja(self, maxX):
+        self.ograniceno = True
+        self.ogranicenjeX = maxX
 
     def pratiIgraca(self):
         lerpSnaga = 0.05
@@ -324,6 +334,9 @@ class Pogled:
             self.x = lerp(self.x, player.x - (self.w - player.width)/2 + ispredHoda*int(player.desno == True) - ispredHoda*int(player.desno == False), lerpSnagaHoda)
         else:
             self.x = lerp(self.x, player.x - (self.w - player.width)/2 + ispredStoji*int(player.desno == True) - ispredStoji*int(player.desno == False), lerpSnaga)
+
+        if self.ograniceno:
+            self.x = max(0, self.x)
 
 pogled = Pogled()
 
--- src/modules/pogled.py
+++ src/modules/pogled.py
@@ -6,10 +6,19 @@ class Pogled:
     y = 0
     w = 240
     h = 136
+    ograniceno = False
+    ogranicenjeX = 0
+
+    def __init__(self):
+        self.postaviOgranicenja(1000)
 
     def prati(self, objekt):
         self.x = objekt.x - (self.w - objekt.width)/2
         #self.y = objekt.y - (self.h - objekt.height)/2
+
+    def postaviOgranicenja(self, maxX):
+        self.ograniceno = True
+        self.ogranicenjeX = maxX
 
     def pratiIgraca(self):
         lerpSnaga = 0.05
@@ -20,5 +29,8 @@ class Pogled:
             self.x = lerp(self.x, player.x - (self.w - player.width)/2 + ispredHoda*int(player.desno == True) - ispredHoda*int(player.desno == False), lerpSnagaHoda)
         else:
             self.x = lerp(self.x, player.x - (self.w - player.width)/2 + ispredStoji*int(player.desno == True) - ispredStoji*int(player.desno == False), lerpSnaga)
+
+        if self.ograniceno:
+            self.x = max(0, self.x)
 
 pogled = Pogled()
